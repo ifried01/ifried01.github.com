8a2a2e108208ffac753a1ed6e653f7e740bd9052
Inbar <inbar.fried@tufts.edu>
2013-04-02 13:16:12 -0400

    removed extra files

--- script.js
+++
@@ -1,9 +0,0 @@
-function init(){
-    canvas = document.getElementById('simple');
-    ctx = canvas.getContext('2d');
-    img = new Image();
-    img.src = 'assets/orange_leather.jpg';
-    ctx.globalAlpha = 0.5; // Transparency
-   // ctx.rotate(0.175); // Angle is in radians
-    ctx.drawImage(img, 100, 100);
-}
